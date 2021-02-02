ab5c283b5cb047d3ea7f5c0bc233c23a233d9822
bobbysaprey420 <bobbysaprey420@gmail.com>
2021-02-03 03:39:22 +0530

    updated

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/index.html'));});
-app.listen(process.env.PORT || 8080);
+let any_path='./dist/your-prep';
+let PORT = process.env.PORT || 5000;
+app.use('/',express.static(path.join(__dirname,any_path)))
+.get('*',(req,res)=>res.sendFile(path.join(__dirname,any_path +'/index.html')))
+.listen(PORT,()=>{
+    console.log(`running on port ${PORT}`);
+})
